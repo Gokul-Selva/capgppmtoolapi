12f8c756d96a5458dada7d2c0a23fe63eddb2f31
gokul <gokulselvakm3@gmail.com>
2020-11-25 12:38:03 +0530

    Added doc on Project Domain

--- src/main/java/com/cg/ppmtoolapi/domain/Project.java
+++ src/main/java/com/cg/ppmtoolapi/domain/Project.java
@@ -1,5 +1,12 @@
 package com.cg.ppmtoolapi.domain;
 
+/**
+*Project Domain is used as data traveller object.
+*All Project validation will be performed here
+*
+*@author Gokul
+*
+*/
 public class Project {
 
 }
